ce881aee0590531a57ba8ca87b85b4606e41b3b5
diya679 <97897457+diya679@users.noreply.github.com>
2022-08-15 19:56:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+function preload(){}
+
+function setup(){
+    canvas=createCanvas(500,500);
+    canvas.center();
+}
+ function draw(){}
+ function take_snapshot(){
+    save('filter1.png');
+ }
